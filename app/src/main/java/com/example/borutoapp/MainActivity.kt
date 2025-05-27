099dfc40c5f2f1072513eea64ba45e006a218046
MohamedAbbas <midooabbas289@gmail.com>
2025-05-27 15:23:53 +0300

    + Splash Screen Animation (new splash api)

--- app/src/main/java/com/example/borutoapp/MainActivity.kt
+++ app/src/main/java/com/example/borutoapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.borutoapp
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.borutoapp.navigation.SetUpNavGraph
@@ -13,6 +17,13 @@ class MainActivity : ComponentActivity() {
     private lateinit var navController: NavHostController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
+        enableEdgeToEdge(
+            navigationBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             navController = rememberNavController()
             SetUpNavGraph(navController = navController)
